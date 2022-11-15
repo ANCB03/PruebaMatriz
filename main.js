7f9cf551216e3ee8feff3ec9904532e9fe019559
ANCB03 <ancabe1403@gmail.com>
2022-11-15 15:25:31 -0500

    Prueba Realizada

---
+++ main.js
@@ -0,0 +1,26 @@
+const M = 3, N = 3; //Tamaño matriz
+if(M%2!=0 && N%2!=0 && M==N){
+var arr = new Array(M);           
+for (var i = 0; i < M; i++) {
+    arr[i] = new Array(N);        
+}
+var num = 1;
+for(var i = 0;i < arr.length;i++){
+    for(var j = 0;j<arr[i].length;j++){
+        arr[i][j]= num;
+        num++;
+    }
+}
+
+var l = arr.length-1;
+for(var k = 0;k<arr.length;k++,l--){
+    if(k!=(arr.length-1)/2 && l!=(arr.length-1)/2){
+        console.log(arr[k][k] +"|"+  arr[k][l]);
+    }else{
+        console.log("-" + arr[k][l] + "-");
+    }
+}}else{
+    console.log("datos erroneos")
+}
+ 
+//console.log(arr);
